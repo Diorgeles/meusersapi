85f6472baa842dc037dd7489addeab8dab4c40ee
Lucas Simon <lucassrod@gmail.com>
2017-03-29 20:23:10 -0300

    Cria os endpoints para usuarios e perfil

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,13 +8,53 @@ from django.conf.urls import url
 
 # Imports from your apps
 from .views import (
-    UserListView
+    UserListView,
+    UserPaginatedView,
+    UserCreateView,
+    UsersMeView,
+    UserDetailCodeView
+)
+
+from .views_profile import (
+    ProfileMeView,
+    ProfileDetailCodeView
 )
 
 urlpatterns = [
     url(
         r'^$',
         UserListView.as_view(),
-        name='users-list'
+        name='list-and-create'
     ),
+    url(
+        r'^paginated/$',
+        UserPaginatedView.as_view(),
+        name='paginated'
+    ),
+    url(
+        r'^me/$',
+        UsersMeView.as_view(),
+        name='me'
+    ),
+    url(
+        r'^me/profile/$',
+        ProfileMeView.as_view(),
+        name='me-profile'
+    ),
+    url(
+        r'^profile/(?P<code>[0-9a-z-]+)/$',
+        ProfileDetailCodeView.as_view(),
+        name='profile-code'
+    ),
+    url(
+        r'^(?P<code>[0-9a-z-]+)/$',
+        UserDetailCodeView.as_view(),
+        name='user-detail-code'
+    ),
+    url(
+        r'^(?P<code>[0-9a-z-]+)/profile/$',
+        UserDetailCodeView.as_view(),
+        name='user-detail-code-profile'
+    ),
+
 ]
