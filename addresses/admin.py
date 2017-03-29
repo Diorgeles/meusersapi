1ed2012e39889a920abbdab6794952538fee5824
Lucas Simon <lucassrod@gmail.com>
2017-03-29 16:43:15 -0300

    Cria admin para o model Address

--- addresses/admin.py
+++ addresses/admin.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from __future__ import unicode_literals
+
 # Stdlib imports
 
 
@@ -10,3 +11,33 @@ from django.utils.translation import ugettext as _
 # Third-party app imports
 
 # Realative imports of the 'app-name' package
+from .models import (
+    Address
+
+)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    """
+    Classe admin para o model de endereços do usuario
+    """
+    list_display = (
+        'user',
+        'street_full_name',
+        'city',
+        'created'
+    )
+
+    def street_full_name(self, obj):
+        return '{},{},{}'.format(
+            obj.street,
+            obj.number,
+            obj.district
+        )
+
+    street_full_name.short_description = 'Endereço completo'
+
+    date_hierarchy = 'created'
+    search_fields = ('street', 'district', 'city', 'created', )
+    list_filter = ('created', )
